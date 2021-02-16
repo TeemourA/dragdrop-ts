af0687aaff9757d1cec1432841c18bac86017e60
TeemourA <taxprogramming@gmail.com>
2021-02-16 22:09:41 +0300

    add inputs validation

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
-const Autobind = (_target: any, _methodName: string, descriptor: PropertyDescriptor) => {
+const Autobind = (
+  _target: any,
+  _methodName: string,
+  descriptor: PropertyDescriptor
+) => {
   const originalMethod = descriptor.value;
   const adjustedDescriptor: PropertyDescriptor = {
     configurable: true,
     get() {
       const boundFn = originalMethod.bind(this);
       return boundFn;
-    }
-  }
+    },
+  };
 
   return adjustedDescriptor;
-}
+};
 
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -19,21 +23,40 @@ class ProjectInput {
   descriptionInputElement: HTMLInputElement;
   peopleInputElement: HTMLInputElement;
 
-
   constructor() {
-    this.templateElement = document.getElementById('project-input') as HTMLTemplateElement;
+    this.templateElement = document.getElementById(
+      'project-input'
+    ) as HTMLTemplateElement;
     this.hostElement = document.getElementById('app') as HTMLDivElement;
 
-    const importedNode = document.importNode(this.templateElement.content, true);
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
     this.element = importedNode.firstElementChild as HTMLFormElement;
     this.element.id = 'user-input';
 
-    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
-    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
-    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+    this.titleInputElement = this.element.querySelector(
+      '#title'
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      '#description'
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      '#people'
+    ) as HTMLInputElement;
 
     this.configure();
     this.attach();
+  }
+
+  private validateBy(validationType: string, ...inputs: string[]) {
+    switch (validationType) {
+      case 'length':
+        return inputs.every(input => input.length !== 0);
+      default:
+        return true;
+    }
   }
 
   private attach() {
@@ -44,10 +67,42 @@ class ProjectInput {
     this.element.addEventListener('submit', this.submitHandler);
   }
 
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescrpition = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (
+      !this.validateBy(
+        'length',
+        enteredTitle,
+        enteredDescrpition,
+        enteredPeople
+      )
+    ) {
+      alert('Please fill all form fields!');
+      return;
+    } else {
+      return [enteredTitle, enteredDescrpition, Number(enteredPeople)];
+    }
+  }
+
+  private clearForm() {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.peopleInputElement.value = '';
+  }
+
   @Autobind
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput;
+      this.clearForm();
+      console.log(title, description, people);
+    }
   }
 }
 
