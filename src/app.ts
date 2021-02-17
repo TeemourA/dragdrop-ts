3d6cbf1221fd601b78a9790d89946937dd0876ad
TeemourA <taxprogramming@gmail.com>
2021-02-17 21:01:46 +0300

    refactor validation & add lists containers

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,38 @@
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+const validate = (input: Validatable) => {
+  let isValid = true;
+
+  if (input.required) {
+    isValid = isValid && input.value.toString().trim().length !== 0;
+  }
+
+  if (input.minLength != null && typeof input.value === 'string') {
+    isValid = isValid && input.value.length >= input.minLength;
+  }
+
+  if (input.maxLength != null && typeof input.value === 'string') {
+    isValid = isValid && input.value.length <= input.maxLength;
+  }
+
+  if (input.min != null && typeof input.value === 'number') {
+    isValid = isValid && input.value >= input.min;
+  }
+
+  if (input.max != null && typeof input.value === 'number') {
+    isValid = isValid && input.value <= input.max;
+  }
+
+  return isValid;
+};
+
 const Autobind = (
   _target: any,
   _methodName: string,
@@ -14,6 +49,38 @@ const Autobind = (
 
   return adjustedDescriptor;
 };
+
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: 'active' | 'finished') {
+    this.templateElement = document.getElementById(
+      'project-list'
+    ) as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app') as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector('ul')!.id = listId;
+    this.element.querySelector('h2')!.textContent = `${this.type.toUpperCase()} PROJECTS`;
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('beforeend', this.element);
+  }
+}
 
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -50,14 +117,14 @@ class ProjectInput {
     this.attach();
   }
 
-  private validateBy(validationType: string, ...inputs: string[]) {
-    switch (validationType) {
-      case 'length':
-        return inputs.every(input => input.length !== 0);
-      default:
-        return true;
-    }
-  }
+  // private validateBy(validationType: string, ...inputs: string[]) {
+  //   switch (validationType) {
+  //     case 'length':
+  //       return inputs.every(input => input.length !== 0);
+  //     default:
+  //       return true;
+  //   }
+  // }
 
   private attach() {
     this.hostElement.insertAdjacentElement('afterbegin', this.element);
@@ -72,18 +139,33 @@ class ProjectInput {
     const enteredDescrpition = this.descriptionInputElement.value;
     const enteredPeople = this.peopleInputElement.value;
 
+    const titleValidation: Validatable = {
+      value: enteredTitle,
+      required: true,
+    };
+
+    const descriptionValidation: Validatable = {
+      value: enteredDescrpition,
+      required: true,
+      minLength: 5,
+    };
+
+    const peopleValidation: Validatable = {
+      value: Number(enteredPeople),
+      required: true,
+      min: 1,
+      max: 5,
+    };
+
     if (
-      !this.validateBy(
-        'length',
-        enteredTitle,
-        enteredDescrpition,
-        enteredPeople
-      )
+      validate(titleValidation) &&
+      validate(descriptionValidation) &&
+      validate(peopleValidation)
     ) {
+      return [enteredTitle, enteredDescrpition, Number(enteredPeople)];
+    } else {
       alert('Please fill all form fields!');
       return;
-    } else {
-      return [enteredTitle, enteredDescrpition, Number(enteredPeople)];
     }
   }
 
@@ -107,3 +189,5 @@ class ProjectInput {
 }
 
 const projectInput = new ProjectInput();
+const activeProjectList = new ProjectList('active');
+const finishedProjectList = new ProjectList('finished');
